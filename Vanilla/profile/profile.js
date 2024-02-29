c98f4d69590d765db77b0cbb17ea6707d9cae133
snow-sr <joaofelipi877@gmail.com>
2024-02-29 15:12:05 -0300

    Login styles + logout feature

--- Vanilla/profile/profile.js
+++ Vanilla/profile/profile.js
@@ -1,3 +1,18 @@
 const header = document.getElementById("Header");
 const profile = localStorage.getItem("login");
 header.innerText = JSON.parse(profile).username;
+
+const btn = document.getElementById("logout");
+
+async function init() {
+  if (!localStorage.getItem("login")) {
+    window.location.href = "http://localhost:5500/Vanilla/login/login.html";
+  }
+}
+
+btn.onclick = () => {
+  localStorage.removeItem("login");
+  init();
+};
+
+init();
